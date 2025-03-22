119467d2b2ee162dd653b220f4903825c95b1d0b
jose.garay <jose.garay@fortyau.com>
2025-03-22 20:29:49 -0300

    delete irrelevant files and functions

--- lib/talent_web/router.ex
+++ lib/talent_web/router.ex
@@ -13,10 +13,6 @@ defmodule TalentWeb.Router do
     plug :fetch_current_user
   end
 
-  pipeline :api do
-    plug :accepts, ["json"]
-  end
-
   scope "/", TalentWeb do
     pipe_through :browser
 
@@ -29,25 +25,14 @@ defmodule TalentWeb.Router do
     live "/dashboard", DashboardLive, :index
   end
 
-  # Other scopes may use custom stacks.
-  # scope "/api", TalentWeb do
-  #   pipe_through :api
-  # end
-
-  # Enable LiveDashboard and Swoosh mailbox preview in development
+  # Enable LiveDashboard in development
   if Application.compile_env(:talent, :dev_routes) do
-    # If you want to use the LiveDashboard in production, you should put
-    # it behind authentication and allow only admins to access it.
-    # If your application does not have an admins-only section yet,
-    # you can use Plug.BasicAuth to set up some basic authentication
-    # as long as you are also using SSL (which you should anyway).
     import Phoenix.LiveDashboard.Router
 
     scope "/dev" do
       pipe_through :browser
 
       live_dashboard "/dashboard", metrics: TalentWeb.Telemetry
-      forward "/mailbox", Plug.Swoosh.MailboxPreview
     end
   end
 
